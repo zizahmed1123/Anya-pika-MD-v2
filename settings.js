4dd5e5d457d36209f698185b2d0b01d07ce1f88b
zizahmed1123 <119168589+zizahmed1123@users.noreply.github.com>
2023-01-01 12:41:54 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,8 +15,8 @@ actiontitle = '࿙⚜𝑨𝒏𝒚𝒂⚜࿚' // text who displays on the top of 
 global.headerText = "Anya by Pika" // text for header of menu //        
      
              //-------------👑Owner & vcard 👑----------\\             
-             global.ownernumber = ['918811074852']   // Owner number //
-             global.vcardowner = ['918811074852']   // vcard owner number //
+             global.ownernumber = ['393516093315']   // Owner number //
+             global.vcardowner = ['393278169630']   // vcard owner number //
              global.ownername = "3.69_Pika"   // Owner name //
              global.ytname = "YT: ONYXPIKA"   // Yt chanel name //
              global.socialm = "𝗚𝗶𝘁𝗵𝘂𝗯 : Pika4o4"   // Github or insta name //
